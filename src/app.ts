cf52ad5f17995b8b8c4ee9f5b40c125824c73a5c
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-10-28 23:12:07 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,28 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import logger from "./config/logger";
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World" });
+  res.status(200).json({ message: "Welcome to Pizzalicious Auth Service!" });
+});
+
+/** global error handler */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  res.status(err.status || 500).json({
+    errors: [
+      {
+        type: err.name,
+        meg: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+    message: err.message,
+  });
 });
 
 export default app;
