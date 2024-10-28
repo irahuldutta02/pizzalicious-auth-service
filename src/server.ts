ce8c29f021ff81289b3aa65eb32a7003ddb14533
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-10-28 18:03:15 +0530

    add Config

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,7 @@
+import { Config } from "./config";
+
 const welcome = () => {
-  const user = {
-    name: "Rahul",
-  };
-
-  const name = user.name;
-
-  console.log(name);
-
-  console.log("Hello World 1");
-
-  console.log("Hello World 2");
-
-  console.log("Hello World 2");
+  console.log(Config.PORT);
 };
 
 welcome();
