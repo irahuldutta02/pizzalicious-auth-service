9faec0781acf32666b9b27a751acfde6682865b8
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-11-10 23:28:42 +0530

    feat(tests): enhance registration tests; add checks for 201 response status and valid JSON response from the endpoint

--- src/tests/users/register.spec.ts
+++ src/tests/users/register.spec.ts
@@ -3,6 +3,7 @@ import app from "../../app";
 
 describe("POST /auth/register", () => {
   describe("Given all fields", () => {
+    // check if the endpoint returns 201
     it("should return 201", async () => {
       // Arrange
       const userData = {
@@ -17,6 +18,25 @@ describe("POST /auth/register", () => {
 
       // Assert
       expect(response.statusCode).toBe(201);
+    });
+
+    // check if the endpoint returns json
+    it("should return valid json response", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Rafael",
+        lastName: "Dias",
+        email: "rdtech2002@gmail.com",
+        password: "123456",
+      };
+
+      // Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      // Assert
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("json"),
+      );
     });
   });
   describe("Fields missing", () => {});
