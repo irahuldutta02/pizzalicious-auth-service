48b3d1d3b68ad4b5f5c1fbb52a530e2111f14390
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-12-08 22:18:53 +0530

    feat(database): add migration scripts for user and refresh token tables; update package.json with migration commands and environment settings

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -11,9 +11,9 @@ export const AppDataSource = new DataSource({
   username: Config.DB_USERNAME,
   password: Config.DB_PASSWORD,
   database: Config.DB_NAME,
-  synchronize: Config.NODE_ENV === "dev" || Config.NODE_ENV === "test",
+  synchronize: Config.NODE_ENV === "test",
   logging: false,
   entities: [User, RefreshToken],
-  migrations: [],
+  migrations: ["src/migration/*.ts"],
   subscribers: [],
 });
